712b70a323b241076dba6d31b500df799ce1ae33
Xavier Stouder <xavier@stouder.io>
2024-03-07 22:25:13 +0100

    fix: correct index content

--- configure.ts
+++ configure.ts
@@ -1,5 +1,6 @@
 import ConfigureCommand from '@adonisjs/core/commands/configure'
 import { stubsRoot } from './stubs/main.js'
+import { readFile, writeFile } from 'node:fs/promises'
 
 export async function configure(command: ConfigureCommand) {
   const codemods = await command.createCodemods()
@@ -11,7 +12,7 @@ export async function configure(command: ConfigureCommand) {
 
   // add directory to rc file
   await codemods.updateRcFile((transformer) =>
-    transformer.setDirectory('audit_resolvers', 'app/audit_resolvers')
+    transformer.addProvider('@stouder-io/adonis-auditing/auditing_provider')
   )
 
   // add default resolvers
@@ -19,4 +20,21 @@ export async function configure(command: ConfigureCommand) {
   await codemods.makeUsingStub(stubsRoot, 'resolvers/user_agent_resolver.stub', {})
   await codemods.makeUsingStub(stubsRoot, 'resolvers/url_resolver.stub', {})
   await codemods.makeUsingStub(stubsRoot, 'resolvers/user_resolver.stub', {})
+
+  // add imports
+  const packageJson = await readFile('package.json', 'utf-8').then(JSON.parse)
+  packageJson.imports = { ...packageJson.imports, '#audit_resolvers/*': './audit_resolvers/*.js' }
+  await writeFile('package.json', JSON.stringify(packageJson, null, 2), { encoding: 'utf-8' })
+
+  // add tsconfig paths
+  const tsConfigJson = await readFile('tsconfig.json', 'utf-8').then(JSON.parse)
+  tsConfigJson.compilerOptions = {
+    ...tsConfigJson.compilerOptions,
+    paths: {
+      ...tsConfigJson.compilerOptions.paths,
+      '#audit_resolvers/*': ['./app/audit_resolvers/*.js'],
+    },
+  }
+  tsConfigJson.compilerOptions.paths['#audit_resolvers/*'] = ['./audit_resolvers/*']
+  await writeFile('tsconfig.json', JSON.stringify(tsConfigJson, null, 2), { encoding: 'utf-8' })
 }
--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
 export { configure } from './configure.js'
+export { defineConfig } from './src/define_config.js'
+export * as errors from './src/errors.js'
+import Auditable from './src/auditable/mixin.js'
+export { Auditable }
